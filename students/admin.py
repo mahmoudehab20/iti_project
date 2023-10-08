50ebae33562a23e1c85a1e9817e067f6fa63df5a
mahmoudehab20 <118760749+mahmoudehab20@users.noreply.github.com>
2023-10-08 15:45:32 +0300

    Delete students directory

--- students/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from students.models import students
-# Register your models here.
-
-
-admin.site.register(students)
-
-
--- students/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-from django.views.generic.edit import CreateView,DeleteView
-from django.views.generic import ListView,UpdateView
-from Admin.models import books
-from students.models import borrowedbooks
