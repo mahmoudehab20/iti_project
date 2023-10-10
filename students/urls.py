043571133193c273bb36c9990ee222a058020205
mahmoudehab20 <118760749+mahmoudehab20@users.noreply.github.com>
2023-10-11 01:05:46 +0300

    Update urls.py

--- students/urls.py
+++ students/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from students.views import (returnbookGenericView,showborrow,bowbooks)
 
 urlpatterns = [
-    path('<int:id>',returnbookGenericView.as_view(),name='book.return'),
+    path('<int:pk>',returnbookGenericView.as_view(),name='book.return'),
     path('borrow/',showborrow,name='borrowed'),
     path('<int:id>/bo',bowbooks,name="borrow.book"),
 ]
